6cf2a4cbe53f8e7ba41a6a8aca7b388c03a81d91
Antonio Chavez <antoniochavezdev@gmail.com>
2024-06-11 17:02:09 -0700

    Adding api prefix again

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import 'reflect-metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.enableCors();
 
   const options = new DocumentBuilder()
