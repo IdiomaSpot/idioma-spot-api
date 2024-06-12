c9dd19ec21829a0c64c8b6abf52b3d60cd763fdb
joselineem <joselineem@gmail.com>
2024-06-12 15:47:21 -0700

    fixing dto and service for class-schedules

--- src/student/class-schedules/dtos/class-schedule.dto.ts
+++ src/student/class-schedules/dtos/class-schedule.dto.ts
@@ -1,15 +1,29 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 
 export class ClassScheduleDTO {
-    @ApiProperty()
-    id: string;
+  @ApiProperty()
+  id: string;
 
-    @ApiProperty()
-    schedule: string;
+  @ApiProperty()
+  schedule: string;
 
-    @ApiProperty()
-    isAlmostFull: boolean;
-    
-    @ApiProperty()
-    isFull: boolean;
+  @ApiProperty()
+  isAlmostFull: boolean;
+
+  @ApiProperty()
+  isFull: boolean;
+
+  @ApiProperty()
+  classLevel: string;
+
+  @Type(() => Date)
+  @ApiProperty()
+  startDate: Date;
+
+  @ApiProperty()
+  hoursDuration: number;
+
+  @ApiProperty()
+  cost: string;
 }
